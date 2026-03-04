af9dc723041684b4b925170d99e46b65def60633
zupa212 <xupiterios808@gmail.com>
2026-03-05 01:48:45 +0200

    fix: Change 'Atelier Location' to 'Shop Location' in ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -108,7 +108,7 @@ export default function ContactForm() {
                         </div>
 
                         <div className="mt-16">
-                            <h4 className="font-heading text-xl text-white mb-4">Atelier Location</h4>
+                            <h4 className="font-heading text-xl text-white mb-4">Shop Location</h4>
                             <address className="not-italic font-serif text-white/60">
                                 8723 SW 132 ST<br />
                                 Miami, FL 33176<br />
